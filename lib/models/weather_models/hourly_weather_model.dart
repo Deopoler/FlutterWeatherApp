38e96652658e750a7ac93e16e1e241026f6058f2
Deopoler <seonjin6804@gmail.com>
2023-03-18 22:57:52 +0900

    feat: add hourly weather service

--- lib/models/weather_models/hourly_weather_model.dart
+++ lib/models/weather_models/hourly_weather_model.dart
@@ -1,1 +1,18 @@
-class HourlyWeatherModel {}
+import 'package:intl/intl.dart';
+
+class HourlyWeatherModel {
+  final double temperature;
+  final int weathercode;
+  late final String svg;
+  final double precipitationProbability;
+  String time;
+
+  HourlyWeatherModel({
+    required this.temperature,
+    required this.weathercode,
+    required this.precipitationProbability,
+    required this.time,
+  }) {
+    time = DateFormat.H().format(DateTime.parse(time));
+  }
+}
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:weather_app/models/weather_models/hourly_weather_model.dart';
 import 'package:weather_app/models/weather_models/today_weather_model.dart';
 import 'package:weather_app/services/geolocator_service.dart';
 
 class WeatherService {
   static const String baseURL =
-      "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,precipitation_probability,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto&current_weather=true";
+      "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,precipitation_probability,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto&current_weather=true&forecast_days=3";
 
   static http.Response? response;
   static String? location;
@@ -27,6 +28,30 @@ class WeatherService {
     if (response!.statusCode == 200) {
       final Map<String, dynamic> weather = jsonDecode(response!.body);
       return TodayWeatherModel.fromJson(weather['current_weather'], location!);
+    }
+    throw Error();
+  }
+
+  static Future<List<HourlyWeatherModel>> getHourlyWeather() async {
+    if (response == null) {
+      await callApi();
+    }
+
+    List<HourlyWeatherModel> weathers = [];
+
+    if (response!.statusCode == 200) {
+      final Map<String, dynamic> weather = jsonDecode(response!.body);
+      final Map<String, dynamic> hourly = weather['hourly'];
+      for (var i in List.generate(hourly['time'].length, (index) => index)) {
+        weathers.add(HourlyWeatherModel(
+          temperature: hourly['temperature_2m'][i],
+          weathercode: hourly['weathercode'][i],
+          precipitationProbability:
+              double.parse(hourly['precipitation_probability'][i].toString()),
+          time: hourly['time'][i],
+        ));
+      }
+      return weathers;
     }
     throw Error();
   }

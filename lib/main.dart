1bf6e29d966f7ca4d97ab4df4634f3bad921815d
Deopoler <seonjin6804@gmail.com>
2023-03-19 22:51:07 +0900

    style: change title

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather Forecast',
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: darkColorScheme,

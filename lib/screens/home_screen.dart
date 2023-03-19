4f0ac1708998b5f124b95da67387f0392f42df5d
Deopoler <seonjin6804@gmail.com>
2023-03-19 22:40:07 +0900

    style: add center

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,64 +24,66 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        constraints: const BoxConstraints(maxWidth: 1100),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FutureBuilder(
-                future: WeatherService.getTodaysWeather(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return TodayCard(
-                      location: snapshot.data!.location,
-                      temperature: snapshot.data!.temperature,
-                      svg: snapshot.data!.svg,
-                    );
-                  } else {
-                    return const LinearProgressIndicator();
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              FutureBuilder(
-                future: WeatherService.getHourlyWeather(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final weather = snapshot.data!;
-                    return SizedBox(
-                      height: 225,
-                      child: ListView.separated(
-                        itemCount: weather.length,
-                        separatorBuilder: (context, index) {
-                          return const SizedBox(
-                            width: 10,
-                          );
-                        },
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          return HourlyCard(
-                            precipitationProbability:
-                                weather[index].precipitationProbability,
-                            svg: weather[index].svg,
-                            temperature: weather[index].temperature,
-                            time: weather[index].time,
-                            index: index,
-                          );
-                        },
-                      ),
-                    );
-                  } else {
-                    return const SizedBox();
-                  }
-                },
-              ),
-            ],
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 1100),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FutureBuilder(
+                  future: WeatherService.getTodaysWeather(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return TodayCard(
+                        location: snapshot.data!.location,
+                        temperature: snapshot.data!.temperature,
+                        svg: snapshot.data!.svg,
+                      );
+                    } else {
+                      return const LinearProgressIndicator();
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FutureBuilder(
+                  future: WeatherService.getHourlyWeather(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final weather = snapshot.data!;
+                      return SizedBox(
+                        height: 225,
+                        child: ListView.separated(
+                          itemCount: weather.length,
+                          separatorBuilder: (context, index) {
+                            return const SizedBox(
+                              width: 10,
+                            );
+                          },
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return HourlyCard(
+                              precipitationProbability:
+                                  weather[index].precipitationProbability,
+                              svg: weather[index].svg,
+                              temperature: weather[index].temperature,
+                              time: weather[index].time,
+                              index: index,
+                            );
+                          },
+                        ),
+                      );
+                    } else {
+                      return const SizedBox();
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
